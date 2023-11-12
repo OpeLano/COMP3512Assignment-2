70452c5a6cba7630de9b487a32a4f22acb8f7978
Shamil Baig <90713044+sbaig853@users.noreply.github.com>
2023-11-11 20:20:28 -0700

    create selectList on the assign2.js file
    and finished values.

--- JSON files/assign2.js
+++ JSON files/assign2.js
@@ -1,13 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+   const artist = JSON.parse(content1);
+   const genres = JSON.parse(content2);
+   const songs = JSON.parse(content3);
 
+   optionSelect('#artist-select', artist);
+   optionSelect('#genre-select', genres);
+   
+   /* url of song api --- https versions hopefully a little later this semester */	
+   const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
 
-/* url of song api --- https versions hopefully a little later this semester */	
-const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
+function optionSelect(idSelector, data) {
+const select = document.querySelector(idSelector);
+
+for(const items of data) {
+   const option = document.createElement('option');
+
+   option.value = items.id;
+   option.textContent = items.name;
+
+   select.appendChild(option);
+}
+
+}
+});
+
 
  
-
-/* note: you may get a CORS error if you try fetching this locally (i.e., directly from a
-   local file). To work correctly, this needs to be tested on a local web server.  
-   Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
-   use built-in Live Preview.
-*/
